db0319f7bf3146c5b22ec17683e41351f262e819
Aleksandr Sysoev <sanchous.ok@gmail.com>
2023-11-12 14:43:39 +0700

    Install ex_vcr

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule CodestatsApi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        vcr: :test,
+        "vcr.delete": :test,
+        "vcr.check": :test,
+        "vcr.show": :test
+      ]
     ]
   end
 
@@ -23,11 +29,13 @@ defmodule CodestatsApi.MixProject do
     [
       {:jason, "~> 1.0"},
       # dev environment
-      {:credo, ">= 0.0.0", only: [:dev], runtime: false},
-      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
-      {:doctor, ">= 0.0.0", only: [:dev], runtime: false},
-      {:ex_check, "~> 0.15.0", only: [:dev], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:doctor, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_check, "~> 0.15.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      # test environment
+      {:exvcr, "~> 0.14", only: :test}
     ]
   end
 end
